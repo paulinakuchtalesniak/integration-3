a766e5b84e28714b00082d066f58998f6fcb066c
Paulina <paulina.kuchta-lesniak@student.howest.be>
2024-01-22 22:41:30 +0100

    timeline desktop

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,40 +1,40 @@
 gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener("resize", adjustTimelineHeight);
-
-function adjustTimelineHeight() {
-  const timeline = document.querySelector(".noopacity-timeline");
-  const items = document.querySelectorAll(".facts-timeline__article");
-  let height = 0;
-
-  items.forEach((item) => {
-    height += item.offsetHeight;
-  });
-
-  timeline.style.height = `${height}px` + `${20}vh`;
+function getSectionHeight() {
+  const section = document.querySelector(".second-section");
+  return section.clientHeight;
 }
 
-adjustTimelineHeight();
+function setupAnimations() {
+  const sectionHeight = getSectionHeight();
+  gsap.to(".moving-circle", {
+    scrollTrigger: {
+      trigger: ".second-section",
+      start: "top 20%",
+      end: "bottom center",
+      scrub: 0.4,
+      // markers: true,
+    },
+    y: sectionHeight, // updated to use the section height
+    ease: "none",
+  });
 
-gsap.to(".moving-circle", {
-  scrollTrigger: {
-    trigger: ".second-section",
-    start: "top 20%",
-    end: "bottom bottom",
-    scrub: 0.1,
-    markers: true,
-  },
-  y: 1600, 
-  ease: "none",
-});
+  gsap.to(".noopacity-timeline rect", {
+    scrollTrigger: {
+      trigger: ".second-section",
+      start: "top 20%",
+      end: "bottom center",
+      scrub: 0.4,
+    },
+    width: sectionHeight, // updated to use the section height
+    ease: "none",
+  });
+}
 
-gsap.to(".noopacity-timeline rect", {
-  scrollTrigger: {
-    trigger: ".second-section",
-    start: "top 20%",
-    end: "bottom bottom",
-    scrub: 0.1,
-  },
-  width: 1600,
-  ease: "none",
+setupAnimations();
+
+// Resize event
+window.addEventListener("resize", () => {
+  setupAnimations(); // Re-setup animations with new height
+  console.log("resize");
 });
