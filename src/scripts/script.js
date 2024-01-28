9aa2e1907f9c5c834dafa631a8f84d7a5d168358
Paulina <paulina.kuchta-lesniak@student.howest.be>
2024-01-28 21:43:01 +0100

    timeline-test

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -23,10 +23,10 @@ const setupAnimations = () => {
     },
   });
 
-  tlTimeline.to(".moving-circle", {
-    y: "300vh",
-    ease: "none",
-  });
+  // tlTimeline.to(".moving-circle", {
+  //   y: "300vh",
+  //   ease: "none",
+  // });
 
   tlTimeline.to(
     ".noopacity-timeline rect",
